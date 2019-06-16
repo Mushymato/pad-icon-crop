2508ab5d023ffa5b6d39a9ce3935b6978ba90099
mushy <wildshinobu@gmail.com>
2019-06-16 00:16:47 -0400

    Updates

---
+++ precache-manifest.f1a921c6a82c2758ba783e3e6aa0461a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f5782566b3a1fb88776fab8d3256f8",
+    "url": "/pad-icon-crop/index.html"
+  },
+  {
+    "revision": "148068e8d04a5062d85e",
+    "url": "/pad-icon-crop/static/css/main.92621cc6.chunk.css"
+  },
+  {
+    "revision": "62c7c062efc3181bfce5",
+    "url": "/pad-icon-crop/static/js/2.b8906bea.chunk.js"
+  },
+  {
+    "revision": "148068e8d04a5062d85e",
+    "url": "/pad-icon-crop/static/js/main.6a56ac52.chunk.js"
+  },
+  {
+    "revision": "1296bfa8c99b986ae4f7",
+    "url": "/pad-icon-crop/static/js/runtime~main.46241204.js"
+  }
+]);
