21ed3221ce37c6193ff570da119d31b5244d11c9
mushymato <wildshinobu@gmail.com>
2020-01-31 00:51:20 -0500

    Updates

---
+++ precache-manifest.24d641c91516ec397674e38111576ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ce89b311cb9ba101bf0fe3802a14c2",
+    "url": "/pad-icon-crop/index.html"
+  },
+  {
+    "revision": "1cfa1a756e65451e4d6f",
+    "url": "/pad-icon-crop/static/css/main.92621cc6.chunk.css"
+  },
+  {
+    "revision": "4ee1750cad8f37831881",
+    "url": "/pad-icon-crop/static/js/2.9ebb5c7f.chunk.js"
+  },
+  {
+    "revision": "4ef0540d32bfda8624fa1dea86b74e04",
+    "url": "/pad-icon-crop/static/js/2.9ebb5c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfa1a756e65451e4d6f",
+    "url": "/pad-icon-crop/static/js/main.e767c3f7.chunk.js"
+  },
+  {
+    "revision": "aba1bc7508f9e35aa940",
+    "url": "/pad-icon-crop/static/js/runtime-main.da0df3e1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pad-icon-crop/precache-manifest.f1a921c6a82c2758ba783e3e6aa0461a.js"
+  "/pad-icon-crop/precache-manifest.24d641c91516ec397674e38111576ed4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pad-icon-crop/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
